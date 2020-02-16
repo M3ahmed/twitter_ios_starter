1e0f4e3f9455c57c671bb67b02dbe71823e66921
Mohamed Ahmed <m3ahmed@ucsd.edu>
2020-02-16 15:14:52 -0800

    almost done ran into an error ill probably work on tomorrow

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -98,6 +98,9 @@ class HomeTableViewController: UITableViewController {
             cell.profileImageView.image = UIImage(data: importData)
         }
         
+        cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
+        cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
+        cell.setRetweeted(tweetArray[indexPath.row]["retweeted"] as! Bool)
         return cell
     }
     
