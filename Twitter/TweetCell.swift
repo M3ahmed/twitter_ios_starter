be60a89969abcfeaf257feec03b34866ab2578dc
Mohamed Ahmed <m3ahmed@ucsd.edu>
2020-02-19 16:18:10 -0800

    fixed the error completely done

--- Twitter/TweetCell.swift
+++ Twitter/TweetCell.swift
@@ -51,6 +51,7 @@ class TweetCell: UITableViewCell {
     func setRetweeted(_ isRetweeted:Bool){
         if(isRetweeted){
             retweetButton.setImage(UIImage(named: "retweet-icon-green"), for: UIControl.State.normal)
+            print("hello")
             retweetButton.isEnabled = false
         }
         else{
